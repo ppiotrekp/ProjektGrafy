e32e327b357bc99d1f8af216be623fdc69e0bfee
Piotr Pyrczak <73071921+ppiotrekp@users.noreply.github.com>
2021-06-01 02:24:19 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,33 +7,7 @@
 
 using namespace std;
 
-bool bfs(int rGraph[V][V], int s, int t, int parent[]){
-    bool visited[V];
-    memset(visited, 0, sizeof(visited));
-    queue<int> q;
-    q.push(s);
-    visited[s] = 1;
-    parent[s] = -1; //element nadrzedny nie istnieje
-
-    while (q.empty()==0) {
-        int u = q.front();
-        q.pop();
-
-        for (int v = 0; v < V; v++) {
-            if (!(visited[v]) && rGraph[u][v] > 0) {
-                if (v == t) {
-                    parent[v] = u; 
-                    return 1;
-                }
-                q.push(v);
-                parent[v] = u;
-                visited[v] = 1;
-            }
-        }
-    }
-
-    return 0;
-}
+bool bfs(int rGraph[V][V], int s, int t, int parent[]);
 
 int fordFulkerson(int graph[V][V], int s, int t){
     int u, v;
@@ -44,10 +18,10 @@ int fordFulkerson(int graph[V][V], int s, int t){
             rGraph[u][v] = graph[u][v];
 
     int parent[V];
-    int max_flow = 0;
+    int max_flow = 0; //maksymalna przepustowosc
 
     while (bfs(rGraph, s, t, parent)) {
-        int path_flow = INT_MAX;
+        int path_flow = INT_MAX; //znajduje maksymalny przeplyw przez znaleziona sciezke
         for (v = t; v != s; v = parent[v]) {
             u = parent[v];
             path_flow = min(path_flow, rGraph[u][v]);
@@ -65,31 +39,62 @@ int fordFulkerson(int graph[V][V], int s, int t){
     return max_flow;
 }
 
-int main(){
+bool bfs(int rGraph[V][V], int s, int t, int parent[]){ //funkcja typu bool zwracająca wartość true jeśli istnieje ścieżka od źródła do ujścia
+    bool searched[V]; //tablica, która oznacza zbadane przez nas wierzchołki, na początku ustawiona na wartosc domyslna czyli 0 zbadanych wierzcholkow
+    memset(searched, 0, sizeof(searched));
+    queue<int> q; //kolejka
+    q.push(s); //dodawanie do kilejki
+    searched[s] = 1;
+    parent[s] = -1; //element nadrzedny nie istnieje
 
-    ifstream myfile ("JSON.txt");
-    if (!myfile.fail())
-    {
-        //cout << "JSON: ";
-        int graph[V][V];
-        string line;
-        string delimiter = ",";
-        int pos = 0;
-        string token;
-//         string table_of_elements[V][V];
-        int i=0;
-        int j=0;
-        while (!myfile.eof())
-        {
-            getline(myfile, line);
-            while ((pos = line.find(delimiter)) != string::npos) {
-                token = line.substr(0,pos);
-                graph[i][j] = stoi(token);
-                line.erase(0, pos+delimiter.length());
-                i++;
+    //przeszukiwanie wszerz
+    while (q.empty()==0) {
+        int u = q.front();
+        q.pop();
+
+        for (int v = 0; v < V; v++) { //jesli znajdziemy polaczenie z wezlem ujscia, ustawiamy element nadrzedny, ktory zwraca true
+            if (!(searched[v]) && rGraph[u][v] > 0) {
+                if (v == t) {
+                    parent[v] = u;
+                    return 1;
+                }
+                q.push(v);
+                parent[v] = u;
+                searched[v] = 1;
             }
-            j++;
         }
+    }
+
+    return 0;
+}
+
+int main(){
+    return 0;
+}
+
+//    ifstream myfile ("JSON.txt");
+//    if (!myfile.fail())
+//    {
+//        //cout << "JSON: ";
+//        int graph[V][V];
+//        string line;
+//        string delimiter = ",";
+//        int pos = 0;
+//        string token;
+//       string table_of_elements[V][V];
+//        int i=0;
+//        int j=0;
+//        while (!myfile.eof())
+//        {
+//            getline(myfile, line);
+//            while ((pos = line.find(delimiter)) != string::npos) {
+//                token = line.substr(0,pos);
+//                graph[i][j] = stoi(token);
+//                line.erase(0, pos+delimiter.length());
+//                i++;
+//            }
+//            j++;
+//        }
 
 //         for (int i=0; i<V; i++) {
 //             for (int j=0; j<V; j++) {
@@ -102,13 +107,3 @@ int main(){
 //    int graph[V][V];
 //
 //
-        string text;
-        std::getline(std::cin, text);
-        cout << text << endl;
-        cout << "The maximum possible flow is " << fordFulkerson(graph, 0, 10);
-        myfile.close();
-    }
-
-    else cout << "Unable to open file";
-
-}
